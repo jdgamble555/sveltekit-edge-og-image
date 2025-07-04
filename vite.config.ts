5990480297f847512e327c5d396dc21f86589aa8
Jonathan Gamble <jdgamble555@gmail.com>
2025-07-03 19:36:48 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import tailwindcss from '@tailwindcss/vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-
+import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson(), wasm()],
+	assetsInclude: ['**/*.wasm'],
 	test: {
 		projects: [
 			{
