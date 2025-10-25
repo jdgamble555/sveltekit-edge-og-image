679a2aac6bde168b9e04ec086e73abb068ba022f
Jonathan Gamble <jdgamble555@gmail.com>
2025-10-25 16:19:01 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,34 +3,9 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-function ogFontRewritePlugin(): Plugin {
-  return {
-    name: 'rewrite-vercel-og-fallback-font',
-    enforce: 'pre',
-    transform(code, id) {
-      // Match the built edge file from @vercel/og that SvelteKit will import
-      if (id.includes('/node_modules/@vercel/og/dist/index.edge.js')) {
-        // Replace the local file fetch with a remote URL fetch
-        const replaced = code.replace(
-          /var fallbackFont = fetch\(new URL\(".*?noto-sans-v27-latin-regular\.ttf", import\.meta\.url\)\)\.then\(\(res\) => res\.arrayBuffer\(\)\);/,
-          'var fallbackFont = fetch("https://cdn.jsdelivr.net/fontsource/fonts/geist-sans@latest/latin-500-normal.woff").then((res) => res.arrayBuffer());'
-          // You can swap that URL to your own static font URL if you don't want to rely on jsdelivr.
-        );
-
-        return {
-          code: replaced,
-          map: null
-        };
-      }
-
-      return null;
-    }
-  };
-}
-
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson(), ogFontRewritePlugin()],
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
 	test: {
 		projects: [
 			{
