00504d7673f55489c63263deaff04a741494c643
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-02 18:59:59 -0600

    wasm test

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
+//import wasm from 'vite-plugin-wasm';
+//import topLevelAwait from 'vite-plugin-top-level-await';
 
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson(), wasm(), topLevelAwait()],
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
 	test: {
 		projects: [
 			{
