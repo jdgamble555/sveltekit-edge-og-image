a3a426dd5cbf7c8354ba992b05a274b4d4144fb0
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-03 16:17:10 -0600

    back to patching

--- vite.config.ts
+++ vite.config.ts
@@ -4,46 +4,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 //import wasm from 'vite-plugin-wasm';
 //import topLevelAwait from 'vite-plugin-top-level-await';
-import wasmModuleWorkers from 'vite-plugin-wasm-module-workers';	
+//import wasmModuleWorkers from 'vite-plugin-wasm-module-workers';	
 //import wasmModuleVercel from './src/lib/wasm-module-vercel';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
-	ssr: {
-		// ⬅️ crucial: bundle the package so the plugin can transform its .wasm import
-		noExternal: ['@resvg/resvg-wasm']
-	},
-	optimizeDeps: {
-		// optional: keep it out of pre-bundling; not required but avoids surprises
-		exclude: ['@resvg/resvg-wasm']
-	},
-	//assetsInclude: ['**/*.wasm?module', '**/*.wasm'],
-	test: {
-		projects: [
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'client',
-					environment: 'browser',
-					browser: {
-						enabled: true,
-						provider: 'playwright',
-						instances: [{ browser: 'chromium' }]
-					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
-	}
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		devtoolsJson()
+	]
 });
