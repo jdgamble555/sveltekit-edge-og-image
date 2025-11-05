94d28aee3c21de2e2260612c90983534c100f7cd
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-04 20:43:00 -0600

    vite wasm

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import tailwindcss from '@tailwindcss/vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-//import wasm from 'vite-plugin-wasm';
+import wasm from 'vite-plugin-wasm';
 //import wasm from 'vite-plugin-wasm-esm';
-//import topLevelAwait from 'vite-plugin-top-level-await';
+import topLevelAwait from 'vite-plugin-top-level-await';
 import { cloudflare } from "@cloudflare/vite-plugin";
 //import wasmModuleWorkers from 'vite-plugin-wasm-module-workers';	
 
@@ -13,10 +13,10 @@ export default defineConfig({
 		tailwindcss(),
 		sveltekit(),
 		devtoolsJson(),
-		cloudflare()
+		cloudflare(),
 		//wasm(['@resvg/resvg-wasm']),
-		//wasm(),
-		//topLevelAwait(),
+		wasm(),
+		topLevelAwait(),
 		//cloudflare()
 	]
 });
