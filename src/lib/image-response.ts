ae57cdb5d296619ccb28b99af64a16d81ced64ec
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-05 19:11:31 -0600

    og image

--- src/lib/image-response.ts
+++ src/lib/image-response.ts
@@ -1,13 +1,13 @@
-import { ImageResponse as VercelOGImageResponse } from '@vercel/og';
+import { ImageResponse as OGImageResponse } from '@cf-wasm/og';
 import { html } from 'satori-html';
 import type { Component } from 'svelte';
 import { render } from 'svelte/server';
 
 
-export class ImageResponse<T extends Record<string, unknown>> extends VercelOGImageResponse {
+export class ImageResponse<T extends Record<string, unknown>> extends OGImageResponse {
     constructor(
         component: Component<T>,
-        options?: ConstructorParameters<typeof VercelOGImageResponse>['1']
+        options?: ConstructorParameters<typeof OGImageResponse>['1']
     ) {
         const result = render(component as Component);
         const element = html(result.body) as React.ReactElement;
