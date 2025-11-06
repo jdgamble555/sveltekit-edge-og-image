5c47f05ebac4bbffafbe2b8ce6614691118198e3
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-05 19:18:21 -0600

    og image

--- src/lib/image-response.ts
+++ src/lib/image-response.ts
@@ -2,6 +2,12 @@ import { ImageResponse as OGImageResponse } from '@cf-wasm/og';
 import { html } from 'satori-html';
 import type { Component } from 'svelte';
 import { render } from 'svelte/server';
+/* @ts-expect-error - font import */
+import notoSansFontBuffer from '@cf-wasm/og/noto-sans-v27-latin-regular.ttf.bin';
+
+import { defaultFont } from '@cf-wasm/og/others';
+
+defaultFont.set(notoSansFontBuffer);
 
 
 export class ImageResponse<T extends Record<string, unknown>> extends OGImageResponse {
