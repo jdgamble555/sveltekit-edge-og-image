ca4d1f62562cf91a1dacb34ceff10f5968837a68
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-12 12:50:10 -0600

    update

--- src/lib/image-response.ts
+++ src/lib/image-response.ts
@@ -1,23 +1,7 @@
 import type { Component } from 'svelte';
 import { render } from 'svelte/server';
 import { t } from "@cf-wasm/og/html-to-react";
-import { ImageResponse as OGImageResponse } from '@cf-wasm/og/others';
-import { initSatori, initYoga } from '@cf-wasm/satori/others';
-import { initResvg } from '@cf-wasm/resvg/others';
-import { defaultFont } from '@cf-wasm/og/others';
-import resvgWasmModule from '@cf-wasm/resvg/resvg.wasm?module';
-import yogaWasmModule from '@cf-wasm/satori/yoga.wasm?module';
-import notoSansFontBuffer from '@cf-wasm/og/noto-sans-v27-latin-regular.ttf.bin';
-
-if (!initSatori.initialized) {
-  initSatori(initYoga(yogaWasmModule));
-}
-
-if (!initResvg.initialized) {
-  initResvg(resvgWasmModule);
-}
-
-defaultFont.set(notoSansFontBuffer);
+import { ImageResponse as OGImageResponse } from '@cf-wasm/og';
 
 
 export class ImageResponse<T extends Record<string, unknown>> extends OGImageResponse {
