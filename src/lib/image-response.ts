d50f5f613e5eb57f303b376971037a96846833fa
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-12 07:40:37 -0600

    test

--- src/lib/image-response.ts
+++ src/lib/image-response.ts
@@ -1,12 +1,13 @@
-import { ImageResponse as OGImageResponse } from '@cf-wasm/og/others';
-import { html } from 'satori-html';
 import type { Component } from 'svelte';
 import { render } from 'svelte/server';
-import notoSansFontBuffer from '@cf-wasm/og/noto-sans-v27-latin-regular.ttf.bin';
-import yogaWasmModule from '@cf-wasm/satori/yoga.wasm?module';
+import { t } from "@cf-wasm/og/html-to-react";
+import { ImageResponse as OGImageResponse } from '@cf-wasm/og/others';
 import { initSatori, initYoga } from '@cf-wasm/satori/others';
-import { initResvg } from '@cf-wasm/resvg/legacy/others';
-import resvgWasmModule from '@cf-wasm/resvg/legacy/resvg.wasm?module';
+import { initResvg } from '@cf-wasm/resvg/others';
+import { defaultFont } from '@cf-wasm/og/others';
+import yogaWasmModule from '@cf-wasm/satori/yoga.wasm?module';
+import resvgWasmModule from '@cf-wasm/resvg/resvg.wasm?module';
+import notoSansFontBuffer from '@cf-wasm/og/noto-sans-v27-latin-regular.ttf.bin';
 
 if (!initSatori.initialized) {
   initSatori(initYoga(yogaWasmModule));
@@ -15,8 +16,6 @@ if (!initSatori.initialized) {
 if (!initResvg.initialized) {
   initResvg(resvgWasmModule);
 }
-
-import { defaultFont } from '@cf-wasm/og/others';
 
 defaultFont.set(notoSansFontBuffer);
 
@@ -27,7 +26,7 @@ export class ImageResponse<T extends Record<string, unknown>> extends OGImageRes
         options?: ConstructorParameters<typeof OGImageResponse>['1']
     ) {
         const result = render(component as Component);
-        const element = html(result.body) as React.ReactElement;
+        const element = t(result.body) as React.ReactElement;
         super(element, options);
     }
 }
