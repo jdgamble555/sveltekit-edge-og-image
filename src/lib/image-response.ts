f888da9f3d4e20865c1ffafb29cdfd51da0d346f
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-13 06:41:52 -0600

    async

--- src/lib/image-response.ts
+++ src/lib/image-response.ts
@@ -1,16 +1,31 @@
 import type { Component } from 'svelte';
 import { render } from 'svelte/server';
 import { t } from "@cf-wasm/og/html-to-react";
-import { ImageResponse as OGImageResponse } from '@cf-wasm/og';
+import { defaultFont, ImageResponse as OGImageResponse } from '@cf-wasm/og';
+import { initResvg } from '@cf-wasm/resvg/legacy/others';
+import resvgWasmModule from '@cf-wasm/resvg/legacy/resvg.wasm?module';
+import { initSatori, initYoga } from '@cf-wasm/satori/others';
+import yogaWasmModule from '@cf-wasm/satori/yoga.wasm?module';
+import notoSansFontBuffer from '@cf-wasm/og/noto-sans-v27-latin-regular.ttf.bin';
 
+export const prerender = false;
 
-export class ImageResponse<T extends Record<string, unknown>> extends OGImageResponse {
-    constructor(
-        component: Component<T>,
-        options?: ConstructorParameters<typeof OGImageResponse>['1']
-    ) {
-        const result = render(component as Component);
-        const element = t(result.body);
-        super(element, options);
-    }
+if (!initResvg.initialized) {
+    initResvg(resvgWasmModule);
 }
+
+if (!initSatori.initialized) {
+    initSatori(initYoga(yogaWasmModule));
+}
+
+defaultFont.set(notoSansFontBuffer);
+
+
+export const ImageResponse = async <T extends Record<string, unknown>>(
+    component: Component<T>,
+    options?: ConstructorParameters<typeof OGImageResponse>['1']
+) => {
+    const result = render(component as Component);
+    return await OGImageResponse.async(t(result.body), options);
+};
+
