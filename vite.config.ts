e94951e6f3c9bde6d873ffd705e2b65901c68854
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-28 21:18:45 -0600

    workers workin

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import cloudflareModules from '@cf-wasm/plugins/vite-cloudflare-modules';
-//import wasm from 'vite-plugin-wasm';
-//import topLevelAwait from 'vite-plugin-top-level-await';
-//import { cloudflare } from "@cloudflare/vite-plugin";
-//import wasm from 'vite-plugin-wasm-esm';
-//import wasmModuleWorkers from 'vite-plugin-wasm-module-workers';	
+
 
 export default defineConfig({
 	ssr: {
@@ -16,7 +11,6 @@ export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
-		devtoolsJson(),
-		cloudflareModules()
+		devtoolsJson()
 	]
 });
